c46fe9ad568cf644942506cd398b555b349cdae2
unknown <dev.render@gmail.com>
2022-11-07 12:36:24 +0200

    country

---
+++ database/migrations/2022_11_07_101943_add_country_to_demos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCountryToDemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('demos', function (Blueprint $table) {
+            //
+            $table->string('country')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('demos', function (Blueprint $table) {
+            //
+        });
+    }
+}
